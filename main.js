ce71f3f7e0ac023bc2eb2cf03688f4ac662de4c3
Ibrahim Eegan <eegan@thepaperidea.com>
2015-12-16 05:06:55 +0500

    Minor changes
    Line added to both sides
    Calendar repeating issue fixed

--- main.js
+++ main.js
@@ -10,15 +10,15 @@ $( document ).ready(function() {
       var place = this.place;
       var date = this.date;
       var color = this.color;
-      $('.calendar').append('<ul class="'+date+'"><li data-color="'+color+'"><a>'+date+'</a><span><p><a>'+name+'</a><a>'+place+'</a></p></span></li><ol></ol></ul>');
+      $('.calendar').append('<ul><li data-color="'+color+'"><a>'+date+'</a><span><p><a>'+name+'</a><a>'+place+'</a></p></span></li><ol></ol></ul>');
       $.each(this.events,function(){
         var name = this.name;
         var person = this.person;
         var time = this.time;
         var timeclass = time.replace(":", "");
-        $('ul.'+date+'>ol','.calendar').append('<li data-color="'+color+'" class="'+timeclass+'"><span><strong>'+time+'</strong><p><a>'+name+'</a></p></span></li>');
+        $('ul:last-child>ol','.calendar').append('<li data-color="'+color+'" class="'+timeclass+'"><span><strong>'+time+'</strong><p><a>'+name+'</a></p></span></li>');
         if(person)
-        $('li.'+timeclass+'>span p','ul.'+date+'>ol').append('<a>'+person+'</a>');
+        $('li.'+timeclass+'>span p','.calendar>ul:last-child>ol').append('<a>'+person+'</a>');
       })
     });
   })
